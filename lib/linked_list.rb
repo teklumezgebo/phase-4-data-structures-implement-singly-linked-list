1797628be57ca27895d87bbb3714120b3862a378
teklumezgebo <mezge002@umn.edu>
2023-08-26 23:24:21 -0500

    commit tree 54baf776d8d47bef5eb2f7151a9988b88d4fd405

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,4 +2,28 @@ require_relative './node'
 
 class LinkedList
 
+    attr_accessor :head
+    
+    def initialize
+        @head = nil
+    end
+
+    def prepend(node)
+
+    end
+
+    def append(node)
+        if head == nil
+            self.head = (node)
+            return
+        end
+
+        last_node = head
+        while last_node.next_node
+            last_node = last_node.next_node
+        end
+        
+        last_node.next_node = node
+    end
+
 end
